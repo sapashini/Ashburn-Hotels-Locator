85cc804142bade38025f24ca2c6b460a9a624e2d
Amin Alhassan <alaasan@yahoo.com>
2019-02-01 16:30:30 -0500

    Updates

---
+++ precache-manifest.9f8e015ffb6cdd2a1b4ecf3c2a215983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dbb0f87a0c538dd9dc7",
+    "url": "/Ashburn-Hotels-Locator/static/js/runtime~main.4dbb0f87.js"
+  },
+  {
+    "revision": "06eb5bba3f60e6a3818f",
+    "url": "/Ashburn-Hotels-Locator/static/js/main.06eb5bba.chunk.js"
+  },
+  {
+    "revision": "c63f57abdfc298840943",
+    "url": "/Ashburn-Hotels-Locator/static/js/1.c63f57ab.chunk.js"
+  },
+  {
+    "revision": "06eb5bba3f60e6a3818f",
+    "url": "/Ashburn-Hotels-Locator/static/css/main.9435dff5.chunk.css"
+  },
+  {
+    "revision": "3fd587beee3f9fb289ebf301cce52879",
+    "url": "/Ashburn-Hotels-Locator/index.html"
+  }
+];
